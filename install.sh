701b384a80e79b92bd1ae9819529a4d36c1d3236
Stephane Busso <stephane.busso@gmail.com>
2015-03-03 08:32:34 +0700

    user messages

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
-if [[ ! -f ~/.docker-stage ]]; then 
+if [[ -f ~/.docker-stage ]]; then 
+  printf "\e[32mupdating docker-stage...\n"
+  rm ~/.docker-stage
   curl -f -L -s https://raw.githubusercontent.com/sbusso/docker-stage/master/init.sh > ~/.docker-stage
-  echo 'source ~/.docker-stage' >> ~/.zshrc
+  source ~/.docker-stage
+  printf "\e[32mdocker-stage updated."
 else
+  printf "\e[32minstalling docker-stage...\n"
   curl -f -L -s https://raw.githubusercontent.com/sbusso/docker-stage/master/init.sh > ~/.docker-stage
+  echo '\nsource ~/.docker-stage' >> ~/.zshrc
+  source ~/.docker-stage
+  printf "\e[32mdocker-stage installed."
 fi
 
-printf "\e[32mdocker-stage installed."
